a58623f4890c12a6019d8d5f6f4f60ffa3b7f5f2
Matias Benary <matiasbenary@gmai.com>
2021-03-10 20:40:54 -0300

    :pencil: Se agrego clase 10/03 formularios dinamicos

---
+++ 39_miercoles10_03/app.js
@@ -0,0 +1,47 @@
+const button = document.getElementById('button');
+const inputText = document.getElementById('inputText');
+const checkbox = document.getElementById('checkbox');
+const title = document.getElementById('title');
+const form = document.getElementById('form');
+//inputText.value -> lo utilizo para obetener el texto,es un string
+//checkbox.checked -> lo utilizo para obetener el estado,es un boolean
+// event -> me da informacion del evento,ademas tiene su propias funciones como preventDefault
+// preventDefault -> evita que se ejecute la accion por defecto
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(event);
+  console.log(inputText.value, checkbox.checked);
+  if (checkbox.checked) {
+    alert('hola como estas');
+  }
+  // se puede cambiar el valor del input:
+  inputText.value = 'Ada';
+});
+
+//focus para cuando el input tiene la atencion,titila el cursor
+inputText.addEventListener('focus', () => {
+  console.log('Focus,Hola como estas');
+});
+
+// es cuando se pierde el focus
+inputText.addEventListener('blur', () => {
+  console.log('Blur,Me despido');
+});
+
+//inputText.value el atributo value, es el valor del input
+
+// input se dispara/ejecuta la accion cuando se cambia el valor del input,ej copio/pego ,toco tecla,etc
+inputText.addEventListener('input', () => {
+  console.log('Input', inputText.value);
+  title.textContent = inputText.value;
+});
+
+// change se dispara/ejecuta la accion cuando se cambia el valor del input pero lo usuario confirma,para esto el usuario apreta el entre o el tab
+inputText.addEventListener('change', () => {
+  console.log('change', inputText.value);
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('El formulario ha sido enviado');
+});
